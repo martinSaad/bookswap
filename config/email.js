f8eaaefb0a78324d7eb6cbfa14994324bcaca108
Martin Saad <martin.saad.ms@gmail.com>
2017-08-21 18:39:10 +0300

    remove sendgrid data

--- config/email.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by martinsaad on 01/08/2017.
- */
-module.exports = {
-    SENDGRID_USERNAME: "app75197953@heroku.com",
-    SENDGRID_PASSWORD: "aeelveij7593",
-    SENDGRID_API_KEY: "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"
-};
